d3398b6385d8bed280cb2ed5f0cf4014a8e5cd19
bubble <1282505255@qq.com>
2023-03-14 18:18:07 +0800

    dump move

--- include/Moves.h
+++ include/Moves.h
@@ -3,6 +3,121 @@
 
 #include "swan/swantypes.h"
 
+#define MUST_HIT   101
+
+#define INFLICT_DURATION_NONE        0
+#define INFLICT_DURATION_PERMANENT   1 // permanent status
+#define INFLICT_DURATION_TRUN        2 // minturn ~ maxturn status
+#define INFLICT_DURATION_POKE        3
+#define INFLICT_DURATION_POKE_TURN   4
+
+#define STAT_NONE       0
+#define STAT_ATK        1
+#define STAT_DEF        2
+#define STAT_SPATK      3
+#define STAT_SPDEF      4
+#define STAT_SPEED      5
+#define STAT_ACCURACY   6
+#define STAT_EVASION    7
+#define STAT_ALL        8
+
+#define SPLIT_STATUS    0x0
+#define SPLIT_PHYSICAL  0x1
+#define SPLIT_SPECIAL   0x2
+
+#define TYPE_NORMAL           0
+#define TYPE_FIGHTING         1
+#define TYPE_FLYING           2
+#define TYPE_POISON           3
+#define TYPE_GROUND           4
+#define TYPE_ROCK             5
+#define TYPE_BUG              6
+#define TYPE_GHOST            7
+#define TYPE_STEEL            8
+#define TYPE_FIRE             9
+#define TYPE_WATER            10
+#define TYPE_GRASS            11
+#define TYPE_ELECTRIC         12
+#define TYPE_PSYCHIC          13
+#define TYPE_ICE              14
+#define TYPE_DRAGON           15
+#define TYPE_DARK             16
+#define TYPE_MYSTERY          17
+
+#define EFFECT_NO_SPECIAL_EFFECT              0
+#define EFFECT_STATUS_INFLICTING              1
+#define EFFECT_TARGET_STAT_CHANGING           2
+#define EFFECT_HEALING                        3
+#define EFFECT_CHANCE_TO_INFLICT_STATUS       4
+#define EFFECT_RAISING_TARGET_STAT            5
+#define EFFECT_DAMAGE_AND_TARGET_STAT_EFFECT  6
+#define EFFECT_DAMAGE_AND_USER_STAT_EFFECT    7
+#define EFFECT_LIFE_STEAL                     8
+#define EFFECT_OHKO                           9
+#define EFFECT_FIELD_EFFCT                    10
+#define EFFECT_SIDE_EFFCT                     11
+#define EFFECT_FORCE_SWITCH_OUT               12
+#define EFFECT_OTHERS                         13
+
+#define FLAG_CONTACT                            (1 << 0)
+#define FLAG_REQUIRES_CHARGE                    (1 << 1)
+#define FLAG_RECHARGE_TURN                      (1 << 2)
+#define FLAG_BLOCKED_BY_PROTECT                 (1 << 3)
+#define FLAG_REFLECTED_BY_MAGIC_COAT            (1 << 4)
+#define FLAG_STOLEN_BY_SNATCH                   (1 << 5)
+#define FLAG_COPIED_BY_MIRROR_MOVE              (1 << 6)
+#define FLAG_PUNCH_MOVE                         (1 << 7)
+#define FLAG_SOUND_MOVE                         (1 << 8)
+#define FLAG_GROUNDED_BY_GRAVITY                (1 << 9)
+#define FLAG_DEFROSTS_TARGETS                   (1 << 10)
+#define FLAG_HITS_NON_ADJACENT_OPPONENTS        (1 << 11)
+#define FLAG_HEALING_MOVE                       (1 << 12)
+#define FLAG_HITS_THROUGH_SUBSTITUTE            (1 << 13)
+
+#define TARGET_OTHER_SELECT         0
+#define TARGET_FRIEND_AND_USER      1
+#define TARGET_FRIEND_SELECT        2
+#define TARGET_ENEMY_SELECT         3
+#define TARGET_OTHER_ALL            4
+#define TARGET_ENEMY_ALL            5
+#define TARGET_FRIEND_ALL           6
+#define TARGET_USER                 7
+#define TARGET_ALL                  8
+#define TARGET_ENEMY_RANDOM         9
+#define TARGET_FIELD                10
+#define TARGET_FIELD_SIDE_ENEMY     11
+#define TARGET_FIELD_SIDE_FRIEND    12
+#define TARGET_UNKNOWN              13
+
+#define STATUS_NONE                 0
+#define STATUS_PARALYZE             1
+#define STATUS_SLEEP                2
+#define STATUS_FREEZE               3
+#define STATUS_BURN                 4
+#define STATUS_POSION               5
+#define STATUS_CONFUSE              6
+#define STATUS_INFATUATE            7
+#define STATUS_BINDING              8
+#define STATUS_NIGHTMARE            9
+#define STATUS_CURSED               10
+#define STATUS_TAUNT                11
+#define STATUS_TORMENT              12
+#define STATUS_DISABLE_LAST_MOVE    13
+#define STATUS_NEXT_TURN_SLEEP      14
+#define STATUS_HEAL_BLOCK           15
+#define STATUS_DISBALE_ABILITY      16
+#define STATUS_FORESIGHT            17
+#define STATUS_LEECH_SEED           18
+#define STATUS_BLOCK_ITEM           19
+#define STATUS_PERISH_SONG          20
+#define STATUS_INGRAIN              21
+
+// did't test
+#define STATUS_CANT_ESCAPE          22
+#define STATUS_ENCORE               23
+
+#define STATUS_SPECIAL_CODE         0xFFFF
+
 typedef struct {
   u8 Type;
   u8 Quality;
@@ -11,7 +126,8 @@ typedef struct {
   u8 Accuracy;
   u8 BasePP;
   char Priority;
-  char HitMinMax;
+  u8 HitMin :4;
+  u8 HitMax :4;
   u16 InflictStatus;
   u8 InflictChance;
   u8 InflictDuration;
@@ -23,9 +139,10 @@ typedef struct {
   char Recoil;
   char Heal;
   u8 Target;
-  char StatChangeStats[3];
+  u8 StatChangeStats[3];
   char StatChangeStages[3];
-  char StatChangeChances[3];
+  u8 StatChangeChances[3];
+  // u16 pad;
   int Flags;
 } MOVE_DATA;
 
